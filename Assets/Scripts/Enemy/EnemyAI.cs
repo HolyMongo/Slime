35e562db1f6633704be1f7295bcdae7b3960cd29
HolyMongo <pererikviktor@gmail.com>
2023-03-27 21:18:35 +0200

    Pathfinding mess
    I imported an asset and did not get it to work

---
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    [Header("Pathfinding")]
+    public Transform target;
+    public float activateDistance = 50f;
+    public float pathUpdateTimer = 0.5f;
+
+    [Header("Physics")]
+    public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+    public float jumpNodeHeightRequirement = 0.8f;
+    public float jumpModifier = 0.3f;
+    public float jumpCheckOffset = 0.1f;
+
+    [Header("Cutom Behavior")]
+    public bool jumpEnabled = true;
+    public bool followEnabled = true;
+    public bool directionLookEnabled = true;
+
+    private Path path;
+    private int currentWaypoint = 0;
+    private bool isGrounded = false;
+    Rigidbody2D rb;
+    Seeker seeker;
+
+
+    private void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, pathUpdateTimer);
+    }
+
+    private void UpdatePath()
+    {
+        if (TargetInDistance() && followEnabled && seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (TargetInDistance() && followEnabled)
+        {
+            PathFollow();
+        }
+    }
+
+    private void PathFollow()
+    {
+        //Does not have a path
+        if (path == null)
+        {
+            return;
+        }
+
+        //Reached end of path
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            return;
+        }
+
+        //see if colliding with anything
+        Vector3 startOffset = transform.position - new Vector3(0f, GetComponent<Collider2D>().bounds.extents.y + jumpCheckOffset);
+        isGrounded = Physics2D.Raycast(startOffset, -Vector3.up, 0.05f);
+
+        //Direction calculation
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        //jump
+        if (jumpEnabled && isGrounded)
+        {
+            if (direction.y > jumpNodeHeightRequirement)
+            {
+                rb.AddForce(Vector2.up * speed * jumpModifier);
+            }
+        }
+
+        //movement
+        rb.AddForce(force);
+
+        //Next waypoint
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        //Direction Graphics Handling
+        if (directionLookEnabled)
+        {
+            if (rb.velocity.x > 0.05f)
+            {
+                transform.localScale = new Vector3(-1f * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+                //transform.localScale = new Vector3(-1f, 1f, 1f); perhaps a better way
+            }
+            else if (rb.velocity.x < -0.05f)
+            {
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+        }
+    }
+
+    private bool TargetInDistance()
+    {
+        return Vector2.Distance(transform.position, target.transform.position) < activateDistance;
+    }
+
+    private void OnPathComplete(Path _p)
+    {
+        if (!_p.error)
+        {
+            path = _p;
+            currentWaypoint = 0;
+        }
+    }
+}
