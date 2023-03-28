e1bf2a170ec67890630e687fa55c657b3ae0f703
HolyMongo <pererikviktor@gmail.com>
2023-03-28 20:09:52 +0200

    More failed attempts
    Failed again but it did work once so I am on the right track

---
+++ Assets/Scripts/Pathfinding Scripts/MouseTarget.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MouseTarget : MonoBehaviour
+{
+    [SerializeField] private Vector3 mousePos;
+    [SerializeField] private List<AIPathController> Bots;
+    [SerializeField] private Transform circle;
+    [SerializeField] private Transform square;
+
+    private void Awake()
+    {
+
+        Bots = FindObjectsOfType<AIPathController>().ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 aux = new Vector3(mousePos.x, mousePos.y, 0);
+            circle.position = aux;
+            Bots.First().Target = circle;
+        }
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //    Vector3 aux = new Vector3(mousePos.x, mousePos.y, 0);
+        //    square.position = aux;
+        //    Bots.Last().Target = square;
+        //}
+    }
+}
