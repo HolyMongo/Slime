685d2a9ab30a4ac88c67dad5dbad83f6e2199e0b
Vidar_Edlund <edlund.vidar@gmail.com>
2023-04-05 21:12:13 +0200

    scenes changed

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -6,13 +6,24 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour
 {
     public string SceneToLoad;
+    public int secBeforeLoadingScene = 1;
+    public Animator anim;
 
-    private void OnEnable()
-    {
-        SceneManager.LoadScene(SceneToLoad);
-    }
     public void LoadScene()
     {
+        StartCoroutine(LoadNextScene());
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(LoadNextScene());
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        anim.SetTrigger("Start");
+
+        yield return new WaitForSeconds(secBeforeLoadingScene);
+
         SceneManager.LoadScene(SceneToLoad);
     }
 }
