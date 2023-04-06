df626d809b186fa3fe9c66c1dde5b771bb8ac1b3
Vidar_Edlund <edlund.vidar@gmail.com>
2023-04-06 02:13:12 +0200

    Build Success

--- Assets/Scripts/Pathfinding Scripts/Extensions/Rigidbody2DExtensions.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public static class Rigidbody2DExtensions
-{
-    public static void AddForceAI(this Rigidbody2D thisObj, Vector2 value)
-    {
-        thisObj.AddForce(value, ForceMode2D.Impulse);
-    }
-}
--- Assets/Scripts/PlayerInRange.cs
+++ Assets/Scripts/PlayerInRange.cs
@@ -6,31 +6,31 @@ public class PlayerInRange : MonoBehaviour
 {
     CircleCollider2D Range;
 
-    private AIPathController pathController;
-    void Start()
-    {
-        Range = GetComponent<CircleCollider2D>();
-        pathController = GetComponent<AIPathController>();
-        pathController.enabled = true;
-    }
+    //private AIPathController pathController;
+    //void Start()
+    //{
+    //    Range = GetComponent<CircleCollider2D>();
+    //    pathController = GetComponent<AIPathController>();
+    //    pathController.enabled = true;
+    //}
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            Range.radius = 20;
-            pathController.enabled = true;
-        }
-    }
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        Range.radius = 20;
+    //        pathController.enabled = true;
+    //    }
+    //}
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            Range.radius = 15;
-            pathController.enabled = false;
-        }
-    }
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        Range.radius = 15;
+    //        pathController.enabled = false;
+    //    }
+    //}
 
 
     void Update()
