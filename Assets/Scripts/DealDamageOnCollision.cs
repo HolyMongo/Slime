49c851d6af31afccb712a2160737c02daf18c1a3
HolyMongo <pererikviktor@gmail.com>
2023-04-14 22:31:50 +0200

    Fixed scripts

--- Assets/Scripts/DealDamageOnCollision.cs
+++ Assets/Scripts/DealDamageOnCollision.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class DealDamageOnCollision : MonoBehaviour
 {
+
+    private GameObject player;
+    private bool isColliding = false;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player"))
         {
-            collision.collider.gameObject.GetComponent<TakeDamage>().GetHit(5, transform.position, 4);
+            player = collision.collider.gameObject;
+            isColliding = true;
         }
     }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            isColliding = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isColliding)
+        {
+            player.GetComponent<TakeDamage>().GetHit(5, transform.position, 0);
+        }
+    }
+
+    
 }
